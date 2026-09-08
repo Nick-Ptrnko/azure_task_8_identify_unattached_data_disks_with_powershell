bbb40d5dbfb5455949313adde735724f38379d67
Mykola Petrenko <mykola.petrenko@gmail.com>
2026-09-08 16:21:19 +0300

    solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,12 @@
 # Write your code here
+# Назва групи ресурсів та вихідного файлу
+$resourceGroupName = "mate-azure-task-5"
+$outputFile = "result.json"
+
+# Отримуємо диски з групи ресурсів та відфільтровуємо лише непідключені
+$unattachedDisks = Get-AzDisk -ResourceGroupName $resourceGroupName | Where-Object {
+    $_.DiskState -eq 'Unattached' -or [string]::IsNullOrEmpty($_.ManagedBy)
+}
+
+# Конвертуємо результат у формат JSON та зберігаємо у result.json
+$unattachedDisks | ConvertTo-Json -Depth 10 | Out-File -FilePath $outputFile -Encoding utf8
